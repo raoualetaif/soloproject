28e99f76f3df5da09bad04fd36338063ca181482
raoualetaif <letaif.raoua@gmail.com>
2021-08-13 06:56:21 +0200

    step1

---
+++ main.js
@@ -0,0 +1,15 @@
+// var arr=[{title:'strawberry',
+// 					src:'strawberry.jpg'},
+// 		{title:'sun',
+// 					src:'sun.jpg'},
+// 		{title:'flower',
+// 					src:'flower.jpg'},
+// 		{title:'dog',
+// 					src:'dog.jpg'},
+// 		{title:'cat',
+// 					src:'cat.jpg'},
+// 		{title:'avocado',
+// 					src:'avocado.jpg'}
+// 					];
+
+
